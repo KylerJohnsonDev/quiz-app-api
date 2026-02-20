f0b7cf12ea01ccda8deb06283b0f75834b5770aa
Kyler Johnson <kylerjohnsondev@gmail.com>
2026-02-20 17:17:41 -0500

    initial commit

---
+++ cmd/api.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type application struct {
+	config config
+	// logger
+	// db driver
+}
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID) // important for rate limiting
+	r.Use(middleware.RealIP)    // important for rate limiting, analytics, and tracing
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(60 * time.Second))
+	r.Use(middleware.Heartbeat("/health"))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello World!"))
+	})
+
+	return r
+}
+
+func (app *application) run(h http.Handler) error {
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      h,
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  10 * time.Second,
+		IdleTimeout:  30 * time.Second,
+	}
+
+	log.Printf("Server has started at http://localhost%s", app.config.addr)
+
+	return srv.ListenAndServe()
+}
+
+type config struct {
+	addr string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	dsn string
+}
+
+// https://quizapi.io/docs/1.0/overview
+// func api() {}
